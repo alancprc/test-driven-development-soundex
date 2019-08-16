e3f64cf9dd720c2fcc0c6ae1e0f60c919d40ab33
alan liang @vapps <alan.liang@xcerra.com>
2019-08-16 11:09:37 +0800

    add PadsWithZerosToEnsureThreeDigits with fixture
    	modified:   soundex-test.cpp

--- soundex-test.cpp
+++ soundex-test.cpp
@@ -1,20 +1,36 @@
 #include <string>
 #include "gmock/gmock.h"
 
-using ::testing::Eq;
+using namespace testing;
 
+std::string padZero(const std::string &str)
+{
+    const int maxLength{4};
+    if (str.length() < 4)
+      return str + std::string(maxLength - str.length(), '0');
+    return str;
+}
 class Soundex
 {
  public:
   std::string encode(const std::string &word) const
   {
-    return word;
+    return padZero(word);
   }
 };
 
-TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
+class SoundexEncoding : public testing::Test
 {
+ public:
   Soundex soundex;
-  auto encoded = soundex.encode("A");
-  ASSERT_THAT(encoded, Eq("A"));
+};
+
+TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
+{
+  ASSERT_THAT(soundex.encode("A"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+  ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
