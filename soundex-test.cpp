528a65da4db703f5e69d0e8388e1d617b68e0523
alan liang @vapps <alan.liang@xcerra.com>
2019-08-16 17:45:32 +0800

    TEST IgnoresVowelLikeLetters passed before coding.

--- soundex-test.cpp
+++ soundex-test.cpp
@@ -87,3 +87,8 @@ TEST_F(SoundexEncoding, LimitsLengthToFourCharacters)
 {
   ASSERT_THAT(soundex.encode("Dcblb").size(), Eq(4u));
 }
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters)
+{
+  ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
