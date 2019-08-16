b258c3404b645965f6f229b81e782ee5829bc893
alan liang @vapps <alan.liang@xcerra.com>
2019-08-16 14:13:23 +0800

    add test ReplacesConsonantsWithAppropriateDigits
    implement encodedDigit() with unordered_map<char, std::string>
    
    	modified:   soundex-test.cpp

--- soundex-test.cpp
+++ soundex-test.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include "gmock/gmock.h"
+#include <unordered_map>
 
 using namespace testing;
 
@@ -12,11 +13,24 @@ std::string padZero(const std::string &str)
 
 std::string head(const std::string &word) { return word.substr(0, 1); }
 
-std::string encodedDigit() { return "1"; }
+std::string encodedDigit(char letter)
+{
+  // clang-format off
+  const std::unordered_map<char, std::string> encodings{
+      {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+      {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"},
+      {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+      {'d', "3"}, {'t', "3"},
+      {'l', "4"},
+      {'m', "5"}, {'n', "5"},
+      {'r', "6"}};
+  // clang-format on
+  return encodings.find(letter)->second;
+}
 
 std::string encodedDigits(const std::string &word)
 {
-  if (word.size() > 1) return encodedDigit();
+  if (word.size() > 1) return encodedDigit(word[1]);
   return "";
 }
 
@@ -45,3 +59,7 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
   ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits)
+{
+  ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
