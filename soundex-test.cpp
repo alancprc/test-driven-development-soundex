3c49802095df8330640f293a7c47dc24b8f31268
alan liang @vapps <alan.liang@xcerra.com>
2019-08-16 11:36:47 +0800

    refactoring encode() with head/encodedDigits

--- soundex-test.cpp
+++ soundex-test.cpp
@@ -5,19 +5,25 @@ using namespace testing;
 
 std::string padZero(const std::string &str)
 {
-    const int maxLength{4};
-    if (str.length() < 4)
-      return str + std::string(maxLength - str.length(), '0');
-    return str;
+  const int maxLength{4};
+  if (str.length() < 4) return str + std::string(maxLength - str.length(), '0');
+  return str;
 }
+
+std::string head(const std::string &word) { return word.substr(0, 1); }
+
+std::string encodedDigits(const std::string &word)
+{
+  if (word.size() > 1) return "1";
+  return "";
+}
+
 class Soundex
 {
  public:
   std::string encode(const std::string &word) const
   {
-    auto encoded = word.substr(0, 1);
-    if (word.size() > 1) encoded += "1";
-    return padZero(encoded);
+    return padZero(head(word) + encodedDigits(word));
   }
 };
 
