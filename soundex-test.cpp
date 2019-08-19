691e2f7a75cb1e9415946a5e5d9925fbc8257436
alan liang @vapps <alan.liang@xcerra.com>
2019-08-19 14:57:04 +0800

    update test IgnoresVowelLikeLetters
    	modified:   soundex-test.cpp

--- soundex-test.cpp
+++ soundex-test.cpp
@@ -102,7 +102,7 @@ TEST_F(SoundexEncoding, LimitsLengthToFourCharacters)
 // TODO test pass before coding against it.
 TEST_F(SoundexEncoding, IgnoresVowelLikeLetters)
 {
-  ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+  ASSERT_THAT(soundex.encode("BaAeEiIoOuUhHyYcdl"), Eq("B234"));
 }
 
 TEST_F(SoundexEncoding, CombinesDuplicateEncodings)
